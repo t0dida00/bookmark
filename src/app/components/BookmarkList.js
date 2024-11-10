0a325d743820dd4c561e5f0e86f1e3ea2b7f7ed2
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-11-10 20:47:47 +0200

    no data show

--- src/app/components/BookmarkList.js
+++ src/app/components/BookmarkList.js
@@ -20,6 +20,11 @@ const BookmarkList = (props) => {
             </div>
             <div className='pt-2 flex flex-col gap-2 relative' >
                 {data && data && data.length > 0 && data.map((item, index) => <Bookmark key={index} data={item} setLoading={setLoading} />)}
+                {!loading && (!data || data.length === 0) && (
+                    <div className='text-gray-500 text-center text-[20px] pt-4'>
+                        No data yet
+                    </div>
+                )}
                 {loading && <Loading />}
 
             </div>
