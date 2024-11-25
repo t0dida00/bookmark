887f2fb85ec732d4e699aaccd43cbc3a984d0f72
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-11-26 00:13:08 +0200

    add image logout

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -47,14 +47,15 @@ const Page = () => {
     if (bookmarksStatus == "succeeded")
         return (
             <div className=' min-h-screen max-w-[840px] flex flex-col pt-[100px] items-center gap-10 m-auto px-2 pb-[50px]'>
-                <div className='absolute top-[50px] left-[50px]'>
-                    <button onClick={() => {
-                        // Clear localStorage
-                        localStorage.clear();
-                        dispatch(logout());
-                        dispatch(clearBookmark())
-                        signOut({ callbackUrl: "/login" });
+                <div className='absolute top-[50px] left-[16px] flex flex-row gap-3 items-center lg:hover:opacity-50'>
+                    <div className='h-[24px] w-[24px] rotate-180'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" /></svg>
 
+                    </div>
+                    <button onClick={() => {
+                        signOut({ callbackUrl: "/login" });
+                        dispatch(logout());
+                        dispatch(clearBookmark());
                     }} className="logout-button">
                         Logout
                     </button>
